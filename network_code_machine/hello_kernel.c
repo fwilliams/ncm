bb78530e10212473700be8082028dc2bac5ed657
Francis Williams <francis@francis-desktop.(none)>
2013-02-09 16:30:32 -0500

    Added basic kernel module that prints time of creation and destruction.

---
+++ network_code_machine/hello_kernel.c
@@ -0,0 +1,18 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/time.h>
+
+struct timeval t;
+
+int init_module() {
+	do_gettimeofday(&t);
+	printk(KERN_INFO "Started at time %lu",
+			(u64) t.tv_sec * 1000 + (u64) t.tv_usec);
+	return 0;
+}
+
+void cleanup_module(void) {
+	do_gettimeofday(&t);
+	printk(KERN_INFO "End time at %lu",
+			(u64) t.tv_sec * 1000 + (u64) t.tv_usec);
+}
