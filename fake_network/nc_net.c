d9fb40a7a935ef60a9fff8243660cabd9dcc855a
Viktor Stanchev <me@viktorstanchev.com>
2013-03-23 23:18:51 -0400

    work in progress...

--- fake_network/nc_net.c
+++ fake_network/nc_net.c
@@ -12,102 +12,59 @@
 #include <linux/syscalls.h>
 #include "nc_net.h"
 
-// http://lxr.free-electrons.com/source/include/linux/skbuff.h#L325
 
-//http://lxr.free-electrons.com/source/net/tipc/eth_media.c#L115
-//http://lxr.free-electrons.com/source/net/batman-adv/send.c#L74
 
-// #define MY_BUFF_SIZE 10
-// #define MY_MAX_DATA_SIZE 20
-
-// typedef struct{
-//   int curr;
-//   int last;
-//   char * data[MY_BUFF_SIZE];
-// }rbuff_tt;
-
-// static int rbuff_set(rbuff_tt *buffer, void * data);
-// static void * rbuff_get(rbuff_tt *buffer);
-// static void rbuff_init(rbuff_tt *buffer);
-
-// static void * tmp;
-
-// static int rbuff_set(rbuff_tt *buffer, void * data) {
-
-//   buffer->data[buffer->last] = data;
-//   buffer->last = ((buffer->last +1) % MY_BUFF_SIZE);
-//   return 0;
-// }
-
-// static void * rbuff_get(rbuff_tt *buffer){
-//   void * tmp;
-
-//   tmp = buffer->data[buffer->curr];
-//   buffer->curr = ((buffer->curr +1) % MY_BUFF_SIZE);
-//   return tmp;
-// }
-
-// static void rbuff_init(rbuff_tt *buff) {
-//   buff->curr = 0;
-//   buff->last = 0;
-// }
-
-// http://www.linuxjournal.com/article/1312
 /*
- void append_frame(char *buf, int len)
- {
- struct sk_buff *skb=alloc_skb(len, GFP_ATOMIC);
- if(skb==NULL)
- my_dropped++;
- else
- {
- skb_put(skb,len);
- memcpy(skb->data,data,len);
- skb_append(&my_list, skb);
- }
- }
- void process_queue(void)
- {
- struct sk_buff *skb;
- while((skb=skb_dequeue(&my_list))!=NULL)
- {
- process_data(skb);
- kfree_skb(skb, FREE_READ);
- }
- }
- */
+ *
+ * TODO in a receiving thread
+ *
+	unsigned char buff[ETH_DATA_LEN];
+	int status;
+	status = nc_rcvmsg(buff, ETH_DATA_LEN);
+ *
+	struct nc_message *tmp;
+	tmp = (struct nc_message *) kmalloc(sizeof(struct nc_message), GFP_KERNEL);
+	tmp->value = *msg;
+	list_add(&(tmp->list), &(chan->message_queue.list));
+ * */
 
-// int num_channels;
-// struct nc_client {
-//   struct nc_message receive_queue;
-//   struct list_head local_clients;
-//   int id;
-// };
-// struct list_head *pos, *q;
-// int nc_clients_num;
-// struct nc_client local_nc_clients;
+#define NUM_CHANNELS sizeof(channels) / sizeof(struct nc_channel)
+
+struct nc_channel channels[] = {
+	{
+		{ {0,0}, /* kernel's list structure */ 0,/*message*/ 0/*length*/ },
+		{ 0x08, 0x00, 0x27, 0xC0, 0x56, 0x5C } /*mac address*/
+	}
+};
+
 
 void init_nc_channel(struct nc_channel *chan) {
 	INIT_LIST_HEAD(&chan->message_queue.list);
 }
 
-// 1 - success, 0 - fail, always succeeds in fake network
-int nc_channel_send(struct nc_channel *chan, u64 *msg) {
-	struct nc_message *tmp;
-	tmp = (struct nc_message *) kmalloc(sizeof(struct nc_message), GFP_KERNEL);
-	tmp->value = *msg;
-	list_add(&(tmp->list), &(chan->message_queue.list));
-	return 1;
+unsigned char our_mac[ETH_ALEN] = { 0x08, 0x00, 0x27, 0xC0, 0x56, 0x5C };
+
+int nc_channel_send(int chan, unsigned char *msg, int length) {
+	struct nc_channel *channel = &channels[chan];
+	if(length > ETH_DATA_LEN){
+		return -1;
+	}
+	return nc_sendmsg(&our_mac, &channel->mac, msg, length);
 }
 
-// 1 - success, 0 - fail, always succeeds in fake network
-int nc_channel_receive(struct nc_channel *chan, u64 *msg) {
+int nc_channel_receive(int chan, int var_id) {
+	struct nc_channel *channel = &channels[chan];
+
+	// pop the first message off the queue
 	struct nc_message *nc_msg =
-			list_first_entry(&chan->message_queue.list, struct nc_message, list);
-	*msg = nc_msg->value;
+			list_first_entry(&channel->message_queue.list, struct nc_message, list);
+
+	// TODO: read value from nc_msg->value of length nc_msg->length into variable var_id
+
+	// delete the message from the queue
 	list_del(&nc_msg->list);
 	kfree(nc_msg);
-	return 1;
+	return 0;
 }
 
 
@@ -121,28 +78,30 @@ void run_tests(void) {
 	init_nc_channel(&chan);
 
 	printk(KERN_INFO "sending: %llu", msg);
-	nc_channel_send(&chan, &msg);
+	nc_channel_send(0, &msg, 8);
 	printk(KERN_INFO "sending: %llu", msg2);
-	nc_channel_send(&chan, &msg2);
+	nc_channel_send(0, &msg2, 8);
 	printk(KERN_INFO "sending: %llu", msg3);
-	nc_channel_send(&chan, &msg3);
+	nc_channel_send(0, &msg3, 8);
 
-	nc_channel_receive(&chan, &msg_rx);
-	printk(KERN_INFO "receiving: %llu", msg_rx);
-	nc_channel_receive(&chan, &msg_rx);
-	printk(KERN_INFO "receiving: %llu", msg_rx);
-	nc_channel_receive(&chan, &msg_rx);
-	printk(KERN_INFO "receiving: %llu", msg_rx);
+//	nc_channel_receive(0, &msg_rx);
+//	printk(KERN_INFO "receiving: %llu", msg_rx);
+//	nc_channel_receive(0, &msg_rx);
+//	printk(KERN_INFO "receiving: %llu", msg_rx);
+//	nc_channel_receive(0, &msg_rx);
+//	printk(KERN_INFO "receiving: %llu", msg_rx);
 }
 
 
 // channels
 
+// queueing
+
 
 
 #define NET_DEVICE_NAME "eth0" // "wlan0" // "eth0"  //sometimes enp0s3
 
-int nc_rcv(unsigned char *buffer, int length) {
+int nc_rcvmsg(unsigned char *buffer, int length) {
 	struct socket *sk = NULL;
 	int i, ret, j;
 	struct msghdr msg;
@@ -277,13 +236,20 @@ int nc_sendmsg(unsigned char* src_mac, unsigned char *dest_mac, unsigned char *m
 }
 
 int init_module(void) {
+	int i;
 	printk(KERN_INFO "Start init...\n");
+	for(i = 0; i < NUM_CHANNELS; i++){
+		init_nc_channel(&channels[i]);
+	}
+//	unsigned char message[] = "hello there";
+//	unsigned char buff[ETH_DATA_LEN];
+//	nc_channel_send(0, message, sizeof(message));
 	unsigned char src_mac[ETH_ALEN] = { 0x08, 0x00, 0x27, 0xC0, 0x56, 0x5C };
 	unsigned char dest_mac[ETH_ALEN] = { 0x30, 0x85, 0xA9, 0x8E, 0x87, 0x95 };
 	unsigned char message[] = "hello there";
 	unsigned char buff[ETH_DATA_LEN];
 	nc_sendmsg(src_mac, dest_mac, message, sizeof(message));
-	nc_rcv(buff, ETH_DATA_LEN);
+//	nc_rcvmsg(buff, ETH_DATA_LEN);
 	printk(KERN_INFO "End init...\n");
 	return 0;
 }
--- fake_network/nc_net.h
+++ fake_network/nc_net.h
@@ -1,19 +1,25 @@
 #include <linux/types.h>
 #include <linux/list.h>
+#include <linux/if_ether.h>
 
 struct nc_message {
   struct list_head list; /* kernel's list structure */
-  u64 value;
+  unsigned char value[ETH_DATA_LEN];
+  int length;
 };
 
 struct nc_channel {
   struct nc_message message_queue;
+  unsigned char mac[ETH_ALEN];
 };
 
 void init_nc_channel(struct nc_channel *chan);
 
-// 1 - success, 0 - fail, always succeeds in fake network
-int nc_channel_send(struct nc_channel *chan, u64 *msg);
+int nc_channel_send(int chan, unsigned char *msg, int length);
 
-// 1 - success, 0 - fail, always succeeds in fake network
-int nc_channel_receive(struct nc_channel *chan, u64 *msg);
+int nc_channel_receive(int chan, int var_id);
+
+
+
+int nc_sendmsg(unsigned char* src_mac, unsigned char *dest_mac, unsigned char *message, int length);
+int nc_rcvmsg(unsigned char *buffer, int length);
