20f4f52a306c354f4a055606ab9acd321ceaff62
Viktor Stanchev <me@viktorstanchev.com>
2013-04-10 19:30:44 -0400

    clean python

--- ncm_translator/mkparams.py
+++ ncm_translator/mkparams.py
@@ -13,8 +13,5 @@ sys.stdout.write(struct.pack('I6s', len(params['channels']),
                  format_mac(params['mac']))+'\x00'*2)
 
 for param in params['channels']:
-    # wirte our own mac
-    # the device name is padded to 16 characters
     sys.stdout.write(struct.pack('16s6s', str(param['dev']),
                      format_mac(param['mac'])))
-    pass
