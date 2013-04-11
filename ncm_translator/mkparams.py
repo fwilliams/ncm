abd0a8f88ebb5028f2f4de2ab62866745f39d77a
Viktor Stanchev <me@viktorstanchev.com>
2013-04-10 20:13:17 -0400

    update params compiler to mote include own mac

--- ncm_translator/mkparams.py
+++ ncm_translator/mkparams.py
@@ -9,9 +9,7 @@ def format_mac(mac):
 
 params = json.load(sys.stdin)
 
-sys.stdout.write(struct.pack('I6s', len(params['channels']),
-                 format_mac(params['mac']))+'\x00'*2)
-
+sys.stdout.write(struct.pack('I', len(params['channels'])))
 for param in params['channels']:
     sys.stdout.write(struct.pack('16s6s', str(param['dev']),
                      format_mac(param['mac'])))
