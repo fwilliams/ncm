5a0a5526d7b5677d89b77707d2081236d62763d0
Viktor Stanchev <me@viktorstanchev.com>
2013-04-11 22:59:33 -0400

    test scripts

---
+++ load_rx1.sh
@@ -0,0 +1,5 @@
+make all install vm=1
+echo "stop" > /sys/network_code/control
+cat samples/rx1.ncm > /sys/network_code/code 
+cat samples/rx1.ncmp > /sys/network_code/params
+echo "start" > /sys/network_code/control
--- vm1_hax.sh
+++ vm1_hax.sh
@@ -1,3 +1,4 @@
 make all install vm=1
 cat /sys/network_code/code > samples/vm1.ncm
+cat /sys/network_code/params > samples/vm1.ncmp
 make uninstall vm=1
