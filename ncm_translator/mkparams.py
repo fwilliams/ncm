cbfdb8c6d7e1aae27ed0df2fca256ed2fdb3139f
Viktor Stanchev <me@viktorstanchev.com>
2013-04-16 00:59:53 -0400

    fix mkparams alignment issue

--- ncm_translator/mkparams.py
+++ ncm_translator/mkparams.py
@@ -9,7 +9,7 @@ def format_mac(mac):
 
 params = json.load(sys.stdin)
 
-sys.stdout.write(struct.pack('I', len(params['channels'])))
+sys.stdout.write(struct.pack('I', len(params['channels'])) + '\x00\x00\x00\x00')
 for param in params['channels']:
     sys.stdout.write(struct.pack('16s', str(param['dev'])))
 for param in params['channels']:
