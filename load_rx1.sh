f48e7861cf77f81e810bcef7fe63ac26cb6cee5a
Viktor Stanchev <me@viktorstanchev.com>
2013-04-16 00:47:49 -0400

    update loading scripts

--- load_rx1.sh
+++ load_rx1.sh
@@ -1,5 +1,8 @@
-make all install vm=1
-echo "stop" > /sys/network_code/control
+make all install
+make -C ncm_translator
+cat samples/rx1.bytecode | ncm_translator/ncm_translator > samples/rx1.ncm
+cat samples/rx1.params.json | python ncm_translator/mkparams.py > samples/rx1.ncmp
+#echo "stop" > /sys/network_code/control
 cat samples/rx1.ncm > /sys/network_code/code 
 cat samples/rx1.ncmp > /sys/network_code/params
 echo "run" > /sys/network_code/control
